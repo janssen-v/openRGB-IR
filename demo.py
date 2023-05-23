682b1de14e5ca023e7968c3a56b38533e765abc8
Vincentius Janssen <janssen.vincentius@gmail.com>
2023-05-23 19:05:54 +0800

    added demo file

--- demo.py
+++ demo.py
@@ -1,42 +1,19 @@
-import os
-import os.path as op
+import isp
 
-import cv2
-import numpy as np
-
-from pipeline import Pipeline
-from utils.yacs import Config
-
-
-OUTPUT_DIR = './output'
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-
-def demo_test_raw():
-    cfg = Config('configs/dark.yaml')
-    pipeline = Pipeline(cfg)
-
-    #bayer = np.load('raw/d65.npy')
+if __name__ == "__main__":
+    bright_path = 'raw/bright/1_1.RAW'
+    dark_path = 'raw/dark/1_1.RAW'
+    speed = 'configs/speed.yaml'
+    quality = 'configs/quality.yaml'
     
-    raw_path = 'raw/bright/1_1.RAW'
-    bayer = np.fromfile(raw_path, dtype='uint16', sep='')
-
-    bayer = bayer.reshape((cfg.hardware.raw_height, cfg.hardware.raw_width))
-
-    data, _ = pipeline.execute(bayer)
-
-    output_path = op.join(OUTPUT_DIR, 'test.png')
-    output = cv2.cvtColor(data['output'], cv2.COLOR_RGB2BGR)
-    cv2.imwrite(output_path, output)
+    mode = speed
     
+    print('Processing d65 color chart...')
+    isp.colorChecker(mode)
     
-    output_path_ir = op.join(OUTPUT_DIR, 'test_ir.png')
-    output_ir = cv2.normalize(data['ir'], dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-    #output_ir = cv2.equalizeHist(output_ir)
-    cv2.imwrite(output_path_ir, output_ir)
-
-
-
-if __name__ == '__main__':
-    print('Processing test raw...')
-    demo_test_raw()
+    print('Processing demo raw (bright/1_1)...')
+    isp.process(bright_path, 'bright', mode)
+    
+    print('Processing demo raw (dark/1_1)...')
+    isp.process(dark_path, 'dark', mode)
+    
